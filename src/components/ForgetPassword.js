40ef68765a3cfee97d0b51c3a6fa4a5dd67e5762
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-09-04 19:30:14 +0530

    added some stylings in the forget page components made it responsive on all the screen sizes

--- src/components/ForgetPassword.js
+++ src/components/ForgetPassword.js
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Stack,Alert} from "react-bootstrap";
+import { Stack, Alert } from "react-bootstrap";
 
 export const ForgetPassword = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [resetStatus, setResetStatus] = useState("");
-    const [showAlert, setShowAlert] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+
   const handlePasswordReset = async () => {
     if (password !== confirmPassword) {
       setResetStatus("Password and confirm password do not match.");
@@ -29,15 +30,44 @@ export const ForgetPassword = () => {
       });
 
       if (response.ok) {
-          setResetStatus("Password reset email sent successfully.");
-          setShowAlert(true);
+        setResetStatus("Password reset email sent successfully.");
+        setShowAlert(true);
       } else {
-          setResetStatus("Password reset failed. Please check your email.");
+        setResetStatus("Password reset failed. Please check your email.");
       }
     } catch (error) {
       console.error("Error resetting password:", error);
       setResetStatus("An error occurred while resetting the password.");
     }
+  };
+
+  const containerStyle = {
+    backgroundColor: "rgba(255, 255, 255, 0.8)",
+    padding: "20px",
+    borderRadius: "8px",
+    boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
+    width: "100%",
+    maxWidth: "400px", // Adjust the maximum width as needed
+    margin: "0 auto", // Center the container horizontally
+  };
+
+  const inputStyle = {
+    width: "100%",
+    padding: "10px",
+    margin: "5px 0",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
+  };
+
+  const buttonStyle = {
+    width: "100%",
+    padding: "10px",
+    backgroundColor: "#007bff",
+    color: "#fff",
+    border: "none",
+    borderRadius: "4px",
+    cursor: "pointer",
+    marginTop: "10px",
   };
 
   return (
@@ -55,34 +85,37 @@ export const ForgetPassword = () => {
             </Nav>
           </Navbar.Collapse>
         </Container>
-          </Navbar>
-          {showAlert && <Alert variant="success" onClose={()=>setShowAlert(false)} dismissible>
-              Your password has been reset successfully,Now you can login with your updated password.
-          </Alert>}
-          <Container>
-               <div>
-                <input
-                  type="email"
-                  placeholder="Enter your email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                  type="password"
-                  placeholder="Enter new password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <input
-                  type="password"
-                  placeholder="Confirm new password"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-                <button onClick={handlePasswordReset}>Reset Password</button>
-              </div>
-              <div>{resetStatus}</div>
-          </Container>
+      </Navbar>
+      {showAlert && <Alert variant="success" onClose={() => setShowAlert(false)} dismissible>
+        Your password has been reset successfully, Now you can log in with your updated password.
+      </Alert>}
+      <Container style={containerStyle}>
+        <div>
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={inputStyle}
+          />
+          <input
+            type="password"
+            placeholder="Enter new password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={inputStyle}
+          />
+          <input
+            type="password"
+            placeholder="Confirm new password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            style={inputStyle}
+          />
+          <button onClick={handlePasswordReset} style={buttonStyle}>Reset Password</button>
+        </div>
+        <div>{resetStatus}</div>
+      </Container>
     </div>
   );
 }
